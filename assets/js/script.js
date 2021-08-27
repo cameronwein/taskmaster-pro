0367e986b2db9f83be20e51a8c61625bdb86e839
Cameron Wein <cameron.wein@gmail.com>
2021-08-27 09:53:54 -0500

    add touchup features

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -193,12 +193,22 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(".list-group").on("sortover", function(event){
+      $(this).addClass("dropover-active");
+    })
   },
   out: function(event) {
+    $(".list-group").on("sortout", function(event){
+      $(this).removeClass("dropover-active");
+    })
   },
   update: function(event) {
 
@@ -238,8 +248,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function(event, ui){
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -262,7 +274,15 @@ var auditTask = function(taskEl){
   else if (Math.abs(moment().diff(time, "days"))<= 2){
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 }
+
+//create interval for audit to run on
+setInterval(function() {
+  $(".card .list-group-item").each(function(index,el){
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
